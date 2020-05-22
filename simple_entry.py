ca2393c6b69d4a3469fd7ed406e1ce6081262f30
Weaver <chris.weaver@bl.uk>
2020-05-22 14:12:32 +0100

    Removed extraneous files from repository

--- simple_entry.py
+++
@@ -1,4 +0,0 @@
-from tkinter.simpledialog import askstring
-from tkinter.messagebox import showinfo
-name = askstring('Name', 'What is your name?')
-showinfo('Hello!', 'Hi, {}'.format(name))
--- testca.py
+++
@@ -1,8 +0,0 @@
-import os
-import ssl                                        
-openssl_dir, openssl_cafile = os.path.split(      
-    ssl.get_default_verify_paths().openssl_cafile)
-# no content in this folder
-os.listdir(openssl_dir)
-# non existent file
-print(os.path.exists(openssl_cafile))
